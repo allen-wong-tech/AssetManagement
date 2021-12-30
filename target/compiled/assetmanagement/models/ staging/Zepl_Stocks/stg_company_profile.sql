303bab3ba2b14983a58f5e981d07bf9c587caddb
Allen Wong <53224894+allen-wong-tech@users.noreply.github.com>
2021-12-30 00:11:04 -0500

    Delete target directory

--- target/compiled/assetmanagement/models/ staging/Zepl_Stocks/stg_company_profile.sql
+++
@@ -1,4 +0,0 @@
-
-
-select symbol, exchange, companyname, industry, website, description, ceo, sector, beta, mktcap::number mktcap
-        from zepl_us_stocks_daily.public.company_profile
--- target/run/assetmanagement/models/ staging/Zepl_Stocks/stg_company_profile.sql
+++
@@ -1,9 +0,0 @@
-
-
-      create or replace transient table finservam.dbt_awong.stg_company_profile  as
-      (
-
-select symbol, exchange, companyname, industry, website, description, ceo, sector, beta, mktcap::number mktcap
-        from zepl_us_stocks_daily.public.company_profile
-      );
-    
